79ec5ff2c4a07c35d852875edcb62a11492e56b9
Hrushi1227 <rushi.ghatol@gmail.com>
2021-01-17 12:35:20 +0530

    React_Practise

--- First/src/Component/Header.js
+++ First/src/Component/Header.js
@@ -3,19 +3,32 @@ import './Header.css'
 
 
 class Header extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            title: 'Devloper Funnel',
+            keyword: 'User Text Here'
+        }
+    }
+
+    Func_name_handle = (event_var) => {
+        console.log(event_var.target.value)
+        this.setState({ keyword: event_var.target.value ? event_var.target.value : 'User Text Here' })
+    }
+
     render() {
         return (
             <Fragment >
 
                 <header>
-                    <div className="logo">Devloper Funnel</div>
+                    <div className="logo">{this.state.title}</div>
                     <center>
-                        <input placeholder='Enter Input'></input>
-                        <div id="user_inp">User Text Here</div>
-                        <hr></hr>
+                        <input placeholder='Enter Input' onChange={this.Func_name_handle}></input>
+                        <div id="user_inp">{this.state.keyword}</div>
                     </center>
                 </header>
-
+                <hr></hr>
             </Fragment >
 
         )
